9c4c596869de6cc35993ca3cccb3cb3fe9b37306
anthonyporturas <anthony.porturas@gmail.com>
2017-03-05 21:50:58 -0500

    final

--- HW2.cu
+++ HW2.cu
@@ -16,13 +16,14 @@
 __global__ void parInterweavedSOE(int * a, int n, int sqrt_N) {
 
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	
+
 	if (threadIdx.x == 0 && threadIdx.y == 0) {
 		a[0] = 1;
 		a[1] = 1;
 	}
+	// Only sqrt(n) processors
 	if (index * index <= n + 1) {
-		for (int i = 2; i * i <= n+1; i++) {
+		for (int i = 2; i * i <= n + 1; i++) {
 			if (a[i] != 1) {
 				for (int j = 2; (index + j)*i < n + 1; j += sqrt_N) {
 					if (index + j >= i) {
@@ -63,9 +64,9 @@ __global__ void parSOE(int * a, int n) {
 //
 void seqSOE(int * a, int n) {
 	a[0] = 1;
-	for (int i = 2; i * i <= n+1; i++) {
+	for (int i = 2; i * i <= n + 1; i++) {
 		if (a[i] != 1) {
-			for (int j = i * i; j <= n+1; j += i) {
+			for (int j = i * i; j <= n + 1; j += i) {
 				a[j] = 1;
 			}
 		}
@@ -130,14 +131,17 @@ int main(int argc, const char * argv[]) {
 	int sqrt_N = int(ceil(sqrt(n)));
 	int threads = 1024; // Standard number of threads
 	int blocks = 32; // Magic value that results in best running speeds and larger acceptable inputs n
-	clock_t start1, start2, end1, end2;
-	int diff, diff2;
+	clock_t start1, start2, start3, end1, end2, end3;
+	int diff1, diff2, diff3;
 
+	// Create and allocate memory for CPU
 	int * a = createArray(n);
 	int * b = createArray(n);
 	int * d_a;
+
+	// Allocate GPU Memory
 	cudaMalloc(&d_a, (n + 1) * sizeof(int));
-	
+
 
 
 	//printPrimes(a, n);
@@ -147,39 +151,46 @@ int main(int argc, const char * argv[]) {
 	seqSOE(b, n);
 
 	end1 = clock();
-	diff = (end1 - start1) * 1000 / CLOCKS_PER_SEC;
+	diff1 = (end1 - start1) * 1000 / CLOCKS_PER_SEC;
 	printf("-----------------------------------\n");
-	printf("Time taken to run the sequential algorithm: %d ms\n", diff);
+	printf("Time taken to run the sequential algorithm: %d ms\n", diff1);
 
 	//printPrimes(b, n);
 
-	//validate(a, b, n);
-
 	start1 = clock();
 	cudaMemcpy(d_a, a, (n + 1) * sizeof(int), cudaMemcpyHostToDevice);
+	end1 = clock();
+	diff1 = (end1 - start1) * 1000 / CLOCKS_PER_SEC;
+
 	//parSOE << <blocks, threads >> > (d_a, n);
+	start2 = clock();
 	parInterweavedSOE << <blocks, threads >> > (d_a, n, sqrt_N);
 	cudaDeviceSynchronize();
-
-	end1 = clock();
-	diff = (end1 - start1) * 1000 / CLOCKS_PER_SEC;
-	printf("-----------------------------------\n");
-	printf("Time taken to run the parallel algorithm: %d ms\n", diff);
-
-	//printPrimes(a, n);
-	start2 = clock();
-	cudaMemcpy(a, d_a, (n + 1) * sizeof(int), cudaMemcpyDeviceToHost);
 	end2 = clock();
 	diff2 = (end2 - start2) * 1000 / CLOCKS_PER_SEC;
-	printf("Time taken to copy memory from GPU onto CPU: %d ms\n", diff2);
-	printf("Total: %d ms\n", diff + diff2);
 	printf("-----------------------------------\n");
+	printf("Time taken to run the parallel algorithm: %d ms\n", diff2);
+
 	//printPrimes(a, n);
+	start3 = clock();
+	cudaMemcpy(a, d_a, (n + 1) * sizeof(int), cudaMemcpyDeviceToHost);
+	end3 = clock();
+	diff3 = (end3 - start3) * 1000 / CLOCKS_PER_SEC;
+	printf("Time taken to copy memory from CPU onto GPU: %d ms\n", diff1);
+	printf("Time taken to copy memory from GPU onto CPU: %d ms\n", diff3);
+	printf("Total: %d ms\n", diff1 + diff2 + diff3);
+	printf("-----------------------------------\n");
+
+	// Uncomment to print out all the primes
+	// printPrimes(a, n);
 
 	validate(a, b, n);
+	printf("-----------------------------------\n");
 
+	// Free GPU Memory
 	cudaFree(d_a);
 
+	// Free CPU Memory
 	free(a);
 	free(b);
 
