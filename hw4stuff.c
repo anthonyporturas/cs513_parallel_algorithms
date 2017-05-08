0b826796bc8311ace1cee79edc96c74fc2cc6dd6
anthonyporturas <anthony.porturas@gmail.com>
2017-05-07 23:12:33 -0400

    1d arrays

--- hw4stuff.c
+++ hw4stuff.c
@@ -8,107 +8,117 @@
 #include <math.h>
 
 
-__global__ void parInterweavedSOE(int * a, int n, int sqrt_N) {
+__device__ void seqMult2(int * output, int tempRow, int commonDim, int tempCol, int *mat1, int *mat2) {
 
-	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	for (int i = 0; i < tempRow * tempCol; i++) {
+		int total = 0;
+		int j = i % tempCol;
 
-	if (threadIdx.x == 0 && threadIdx.y == 0) {
-		a[0] = 1;
-		a[1] = 1;
-	}
-	if (index * index <= n + 1) {
-		for (int i = 2; i * i <= n + 1; i++) {
-			if (a[i] != 1) {
-				for (int j = 2; (index + j)*i < n + 1; j += sqrt_N) {
-					if (index + j >= i) {
-						a[(index + j) * i] = 1;
-					}
-				}
-				__syncthreads();
-			}
+		for (int k = 0; k < commonDim; k++) {
+			total += mat1[k + (i / tempCol)*commonDim] * mat2[(k*tempCol + j)];
 		}
-	}
-}
-__device__ void seqMult2(int ** output, int tempRow, int commonDim, int tempCol, int **mat1, int **mat2) {
-	int i, j;
 
-	for (i = 0; i < tempRow; i++) {
-		for (j = 0; j < tempCol; j++) {
-			int total = 0;
-			for (int multNum = 0; multNum < commonDim; multNum++) {
-				total += mat1[i][multNum] * mat2[multNum][j];
-			}
-			output[i][j] = total;
-		}
+		output[i] = i;
+
 	}
 
 
 }
-__global__ void parMat(int * dimVect, int numDim, int *** matList, int *** intermediate, int levels) {
+__global__ void parMat2(int * dimVect, int numDim, int ** matList, int ** intermediate, int levels) {
+	/*
+	for (int i = 0; i < numDim - 2; i++) {
+		intermediate[i][0] = 1;
+		intermediate[i][1] = 2;
+	}
+	*/
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	int ind = 2;
+
+	int commonDim = dimVect[1];
+	int tempRow = dimVect[0];
+	int tempCol = dimVect[2];
+	int *mat1 = matList[0];
+	int *mat2 = matList[1];
+	int *output = new int[tempRow*tempCol];
+	seqMult2(output, tempRow, commonDim, tempCol, mat1, mat2);
 	int threadMax = ((numDim - 2) / 2) + ((numDim - 2) % 2);
-	
+	threadMax = 100;
+	/*
+	if (index > threadMax) {
+		intermediate[0][index % (tempRow*tempCol)] = output[index % (tempRow*tempCol)];
+	}
+	*/
+	intermediate[0][0] = output[0];
+	intermediate[0][1] = output[1];
+	intermediate[0][2] = output[2];
+	intermediate[0][3] = output[3];
+
+}
+__global__ void parMat(int * dimVect, int numDim, int ** matList, int ** intermediate, int levels) {
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int threadMax = ((numDim - 2) / 2) + ((numDim - 2) % 2);
+	if (index == 0) {
+		return;
+	}
 	int tempRow, tempCol, commonDim;
 	for (int i = 0; i < levels; i++) {
-		if (index < threadMax) {
+		if (index <= threadMax) {
 
-			if (i == 0) {
-				tempRow = dimVect[index*2-1];
-				tempCol = dimVect[index*2 + 1];
-				commonDim = dimVect[index*2];
-				int **mat1 = matList[index * 2 - 1];
-				int **mat2 = matList[index * 2];
-				int **output = (int **)malloc(tempRow * sizeof(int *));
-				for (int k = 0; k < tempRow; k++) {
-					output[k] = (int *)malloc(tempCol * sizeof(int));
-				}
+			if (i == 1) {
+				tempRow = dimVect[index * 2 - 2];
+				tempCol = dimVect[index * 2];
+				commonDim = dimVect[index * 2 - 1];
+				int *mat1 = matList[index * 2 - 2];
+				int *mat2 = matList[index * 2 - 1];
+				//int *output = (int *)malloc(tempRow * tempCol* sizeof(int ));
+				int *output = new int[tempRow*tempCol];
+				
 				seqMult2(output, tempRow, commonDim, tempCol, mat1, mat2);
 				intermediate[index] = output;
 			}
 			else {
 				commonDim = tempCol;
-				tempCol = dimVect[index * 2 + (i * 2)];
-				int **mat1 = intermediate[index];
-				int **mat2 = intermediate[index + 1];
-				int **output = (int **)malloc(tempRow * sizeof(int *));
-				for (int k = 0; k < tempRow; k++) {
-					output[k] = (int *)malloc(tempCol * sizeof(int));
-				}
+				tempCol = dimVect[index * 2 + (i * 2) - 1];
+				int *mat1 = intermediate[index];
+				int *mat2 = intermediate[index + 1];
+				//int *output = (int *)malloc(tempRow * tempCol* sizeof(int ));
+				int *output = new int[tempRow*tempCol];
 				__syncthreads();
 				seqMult2(output, tempRow, commonDim, tempCol, mat1, mat2);
 				intermediate[index] = output;
 			}
 			__syncthreads();
 		}
-		ind = ind * 2;
+
 		threadMax = ((threadMax - 2) / 2) + ((threadMax - 2) % 2);
 	}
-	
+	__syncthreads();
+	if (index == 1) {
+		intermediate[0] = intermediate[1];
+	}
 
 }
 
 
 __global__ void parMult(int** output, int **mat1, int **mat2, int row, int com, int col) {
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	
+
 	int i, j;
-	
-		output = (int **)malloc(row * sizeof(int *));
-		for (i = 0; i < row; i++) {
-			output[i] = (int *)malloc(col * sizeof(int));
-		}
 
-		i = index / col;
-		j = (index % col) - 1;
-		int total = 0;
-		for (int multNum = 0; multNum < com; multNum++) {
-			total += mat1[i][multNum] * mat2[multNum][j];
-		}
+	output = (int **)malloc(row * sizeof(int *));
+	for (i = 0; i < row; i++) {
+		output[i] = (int *)malloc(col * sizeof(int));
+	}
 
-		output[i][j] = total;
-			
-		
+	i = index / col;
+	j = (index % col) - 1;
+	int total = 0;
+	for (int multNum = 0; multNum < com; multNum++) {
+		total += mat1[i][multNum] * mat2[multNum][j];
+	}
+
+	output[i][j] = total;
+
+
 
 }
 
@@ -116,22 +126,15 @@ __global__ void parMult(int** output, int **mat1, int **mat2, int row, int com, 
 // Create random matrices given row and column sizes
 // Right now for testing, only fills in incremented values
 // Needs to be int values
-int ** randMat(int row, int col) {
+int * randMat(int row, int col) {
 	int i, j;
 	int count;
-	int **arr = (int **)malloc(row * sizeof(int *));
-	for (i = 0; i<row; i++)
-		arr[i] = (int *)malloc(col * sizeof(int));
+	int *arr = (int *)malloc(row * col* sizeof(int ));
 
-	// Note that arr[i][j] is same as *(*(arr+i)+j)
 	count = 0;
-	for (i = 0; i < row; i++) {
-		for (j = 0; j < col; j++) {
-			//printf("%d ", count);
-			//printf("\n");
-			arr[i][j] = ++count;  // OR *(*(arr+i)+j) = ++count
-			//printf("%d ", arr[i][j]);
-		}
+	for (i = 0; i < row*col; i++) {
+			arr[i] = ++count;  
+		
 	}
 
 	return arr;
@@ -143,11 +146,11 @@ int ** randMat(int row, int col) {
 // m is the number of columns in the first matrix
 // n is the number of columns in the second matrix
 // d is the number of matrices to multiply
-int ** seqMult(int * dimVect, int dimNum, int ***b) {
+int * seqMult(int * dimVect, int dimNum, int **b) {
 	int i, j, k;
-	int **output = b[0];
-	int **mat1;
-	int **mat2;
+	int *output = b[0];
+	int *mat1;
+	int *mat2;
 	for (k = 0; k < dimNum - 3; k++) {
 		int tempRow;
 		if (k == 0) {
@@ -162,143 +165,135 @@ int ** seqMult(int * dimVect, int dimNum, int ***b) {
 
 		int tempCol = dimVect[k + 2];
 		int commonDim = dimVect[k + 1];
-		output = (int **)malloc(tempRow * sizeof(int *));
-		for (i = 0; i < dimVect[k]; i++) {
-			output[i] = (int *)malloc(tempCol * sizeof(int));
-		}
+		output = (int *)malloc(tempRow * tempCol*sizeof(int ));
 
-		for (i = 0; i < tempRow; i++) {
-			for (j = 0; j < tempCol; j++) {
+		for (i = 0; i < tempRow * tempCol; i++) {
 				int total = 0;
-				for (int multNum = 0; multNum < commonDim; multNum++) {
-					total += mat1[i][multNum] * mat2[multNum][j];
+				int j = i % tempCol;
+				
+				for (int k = 0; k < commonDim; k++) {
+					total += mat1[k+(i/ tempCol)*commonDim] * mat2[(k*tempCol + j)];
 				}
-				output[i][j] = total;
-			}
+
+				output[i] = total;
+			
 		}
 
 	}
 	return output;
 }
 
+#include <cstdio>
+inline void GPUassert(cudaError_t code, char * file, int line, bool Abort = true)
+{
+	if (code != 0) {
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (Abort) exit(code);
+	}
+}
+
+#define GPUerrchk(ans) { GPUassert((ans), __FILE__, __LINE__); }
+
+
+
+__global__ void doSmth(int*** a) {
+	for (int i = 0; i<2; i++)
+		for (int j = 0; j<2; j++)
+			for (int k = 0; k<2; k++)
+				a[i][j][k] = i + j + k;
+}
+
+
 int main(int argc, const char * argv[]) {
-	
+
 	// Check to see if there is at least one matrix
 	if (argc < 3) {
 		printf("The argument is wrong! Execute your program with a vector with at least two integers.\n");
 		return 1;
 	}
-	
+
 	int i, j, k;
 	int threads = 1024; // Standard number of threads
 	int blocks = 32; // Magic value that results in best running speeds and larger acceptable inputs n
 
-	// Arary holding all the dimensions
+					 // Arary holding all the dimensions
 	int * dimVect = (int *)malloc((argc - 1) * sizeof(int));
 	for (i = 1; i < argc; i++) {
 		dimVect[i - 1] = atoi(argv[i]);
 	}
 
-	int **a; // Temporary 2D array to hold current matrix
-	int ***b = (int ***)malloc((argc - 1) * sizeof(int**)); // Array holding all matrices
-	for (i = 0; i < argc-2; i++) {
-		a = randMat(dimVect[i], dimVect[i+1]);
+	int *a; // Temporary 2D array to hold current matrix
+	int **b = (int **)malloc((argc - 1) * sizeof(int*)); // Array holding all matrices
+	for (i = 0; i < argc - 2; i++) {
+		a = randMat(dimVect[i], dimVect[i + 1]);
 		b[i] = a;
 	}
 
 
 	// Print out all matrices
 	for (k = 0; k < argc - 2; k++) {
+		printf("Mat %d: ", k + 1);
 		a = b[k];
 		int tempRow = dimVect[k];
 		int tempCol = dimVect[k + 1];
-		for (i = 0; i < tempRow; i++)
-			for (j = 0; j < tempCol; j++)
-				printf("%d ", a[i][j]);
+		for (i = 0; i < tempRow*tempCol; i++)
+				printf("%d ", a[i]);
 		printf("\n");
 	}
-	
-	//int ***intermediate = (int ***)malloc((argc - 1) * sizeof(int**));
 
-	// Call Sequential Multiplication
-	int **result = seqMult(dimVect, argc, b);
-	for (i = 0; i < dimVect[0]; i++)
-		for (j = 0; j < dimVect[argc - 2]; j++)
-			printf("%d ", result[i][j]);
+	int *result = seqMult(dimVect, argc, b);
+	printf("Sequential Result:\n");
+	for (int i = 0; i < dimVect[0] * dimVect[argc - 2]; i++) {
+		printf("%d ", result[i]);
+	}
 	printf("\n");
-	int levels = log(argc - 2);
+	printf("\n");
 
 	int * d_dimVect;
-	cudaMalloc(&d_dimVect, (argc-1) * sizeof(int));
-	cudaMemcpy(d_dimVect, dimVect, (argc-1) * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMalloc((void **)&d_dimVect, (argc-1) * sizeof(int));
+	cudaMemcpy(d_dimVect, dimVect, (argc - 1) * sizeof(int), cudaMemcpyHostToDevice);
 
-	int *** matList;
-	cudaMalloc(&matList, (argc - 2) * sizeof(int**));
-	cudaMemcpy(matList, b, (argc - 2) * sizeof(int**), cudaMemcpyHostToDevice);
+	int ** matList = (int **)malloc((argc-2) * sizeof(int *));
+	for (i = 0; i < (argc-2); i++) {
+		cudaMalloc((void **)&matList[i], dimVect[i] * dimVect[i + 1] * sizeof(int));
+		cudaMemcpy(matList[i], b[i], dimVect[i] * dimVect[i + 1] * sizeof(int), cudaMemcpyHostToDevice);
+	}
 
-	int ***out = (int ***)malloc((((argc - 2) / 2) + 1) * sizeof(int**));
-	
+	int ** d_matList;
+	cudaMalloc(&d_matList, (argc - 2) * sizeof(int *));
+	cudaMemcpy(d_matList, matList, (argc - 2) * sizeof(int *), cudaMemcpyHostToDevice);
 
-	int *** intermediate;
-	cudaMalloc(&intermediate, (((argc - 2)/2)+1) * sizeof(int**));
-	cudaMemcpy(intermediate, out, (((argc - 2) / 2) + 1) * sizeof(int**), cudaMemcpyHostToDevice);
+
+	int ** interMat = (int **)malloc(ceil((argc - 2) / 2) + 1 * sizeof(int *));
+	int * currentMat = (int *)calloc(100, sizeof(int));
+	for (i = 0; i < ceil((argc-2)/2)+1; i++) {
+		cudaMalloc((void **)&interMat[i], 100 * sizeof(int));
+		cudaMemcpy(interMat[i], currentMat, 100 * sizeof(int), cudaMemcpyHostToDevice);
+	}
+
+	int ** d_interMat;
+	cudaMalloc(&d_interMat, (argc - 2) * sizeof(int *));
+	cudaMemcpy(d_interMat, interMat, (argc - 2) * sizeof(int *), cudaMemcpyHostToDevice);
+
+	int levels = log(argc - 2) / log(2);
+	parMat2 << <blocks, threads >> > (d_dimVect, argc, d_matList, d_interMat, levels);
+
 	/*
-	for (int z = 0; z < (((argc - 2) / 2) + 1); z++) {
-		cudaMalloc(&intermediate[z], (((argc - 2) / 2) + 1) * sizeof(int*));
+	int *res = (int *)malloc(100 * 100 * sizeof(int));
+	for (i = 0; i < ceil((argc - 2) / 2) + 1; i++) {
+		printf("1:\n");
+		cudaMemcpy(res, d_interMat[i], 100 * 100 * sizeof(int), cudaMemcpyDeviceToHost);
+		printf("2:\n");
+		b[i] = res;
 	}
 	*/
-	parMat << <blocks, threads >> > (d_dimVect, argc, matList, intermediate, levels);
-	
-	cudaMemcpy(out, intermediate, (argc-2) * sizeof(int**), cudaMemcpyDeviceToHost);
 
-	cudaFree(d_dimVect);
-	cudaFree(matList);
-	cudaFree(intermediate);
-	
-	int **result2 = out[0];
-	//printf("%d \n", result[0][0]);
-	/**************************************
-	
-	int **output = b[0];
-	int **mat1;
-	int **mat2;
-	int **d_mat1, **d_mat2;
-	for (k = 0; k < dimNum - 3; k++) {
-		int tempRow;
-		if (k == 0) {
-			mat1 = b[k];
-			tempRow = dimVect[k];
-		}
-		else {
-			mat1 = output;
-		}
+	int res[2][2];
+	for (int i = 0; i<2; i++)
+			GPUerrchk(cudaMemcpy(&res[i][0], interMat[i], 4*sizeof(int), cudaMemcpyDeviceToHost));
 
-		mat2 = b[k + 1];
-
-		int tempCol = dimVect[k + 2];
-		int commonDim = dimVect[k + 1];
-
-		cudaMalloc(&d_output, tempRow * sizeof(int*));
-		cudaMalloc(&d_mat1, tempRow * sizeof(int*));
-		cudaMalloc(&d_mat2, commonDim * sizeof(int*));
-		cudaMemcpy(d_mat1, mat1, tempRow * sizeof(int*), cudaMemcpyHostToDevice);
-		cudaMemcpy(d_mat2, mat2, commonDim * sizeof(int*), cudaMemcpyHostToDevice);
-		parMult << <blocks, threads >> > (d_a, n, sqrt_N);
-
-	}
-	
-	
-	cudaMemcpy(b, d_b, (argc-1) * sizeof(int**), cudaMemcpyDeviceToHost);
-	***********************************************/
-
-	// Check Results
-	
-	for (i = 0; i < dimVect[0]; i++)
-		for (j = 0; j < dimVect[argc-2]; j++)
-			printf("%d ", result2[i][j]);
-	printf("\n");
-	
-	//cudaFree(d_b);
-	
-	return 0;	
+	for (int i = 0; i<2; i++)
+		for (int j = 0; j<4; j++)
+				printf("[%d][%d]=%d\n", i, j, res[i][j]);
+	return 0;
 }
