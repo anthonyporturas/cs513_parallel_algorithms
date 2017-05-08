f24d94ae700b215321692c09027dc84fe0634cbf
anthonyporturas <anthony.porturas@gmail.com>
2017-05-07 23:50:40 -0400

    dwada

--- hw4seq.cu
+++ hw4seq.cu
@@ -25,8 +25,8 @@ __device__ void seqMult2(int * output, int tempRow, int commonDim, int tempCol, 
 __global__ void parMat2(int * dimVect, int numDim, int ** matList, int ** intermediate, int levels) {
 	/*
 	for (int i = 0; i < numDim - 2; i++) {
-	intermediate[i][0] = 1;
-	intermediate[i][1] = 2;
+		intermediate[i][0] = 1;
+		intermediate[i][1] = 2;
 	}
 	*/
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
@@ -42,7 +42,7 @@ __global__ void parMat2(int * dimVect, int numDim, int ** matList, int ** interm
 	threadMax = 100;
 	/*
 	if (index > threadMax) {
-	intermediate[0][index % (tempRow*tempCol)] = output[index % (tempRow*tempCol)];
+		intermediate[0][index % (tempRow*tempCol)] = output[index % (tempRow*tempCol)];
 	}
 	*/
 	intermediate[0][0] = output[0];
@@ -69,7 +69,7 @@ __global__ void parMat(int * dimVect, int numDim, int ** matList, int ** interme
 				int *mat2 = matList[index * 2 - 1];
 				//int *output = (int *)malloc(tempRow * tempCol* sizeof(int ));
 				int *output = new int[tempRow*tempCol];
-
+				
 				seqMult2(output, tempRow, commonDim, tempCol, mat1, mat2);
 				intermediate[index] = output;
 			}
@@ -139,17 +139,17 @@ __global__ void parMult(int** output, int **mat1, int **mat2, int row, int com, 
 
 int * createMatrix(FILE* file, int row, int col) {
 
-	int i, j;
-	int count;
-	int *arr = (int *)malloc(row * col * sizeof(int));
+    int i, j;
+    int count;
+    int *arr = (int *)malloc(row * col * sizeof(int));
 
-	count = 0;
-	for (i = 0; i < row*col; i++) {
-		fscanf(file, "%d", &arr[i]);
+    count = 0;
+    for (i = 0; i < row*col; i++) {
+        fscanf(file, "%d", &arr[i]);
 
-	}
+    }
 
-	return arr;
+    return arr;
 }
 
 // The sequential matrix multiplication version
@@ -159,163 +159,210 @@ int * createMatrix(FILE* file, int row, int col) {
 // n is the number of columns in the second matrix
 // d is the number of matrices to multiply
 int ** seqMult(int * dimVect, int dimVectLen, int ***b) {
-	int i, j, k;
-	int **output = b[0];
-	int **mat1;
-	int **mat2;
-	printf("AAA\n");
-	for (k = 0; k < dimVectLen - 2; k++) {
-		printf("k: %d\n", k);
+    int i, j, k;
+    int **output = b[0];
+    int **mat1;
+    int **mat2;
+    printf("AAA\n");
+    for (k = 0; k < dimVectLen - 2; k++) {
+        printf("k: %d\n", k);
 
-		// printf("AAA\n");
-		int tempRow;
-		if (k == 0) {
-			mat1 = b[k];
-			tempRow = dimVect[k];
-			// printf("BBB\n");
-		}
-		else {
-			mat1 = output;
-		}
-		// printf("CCC\n");
-		mat2 = b[k + 1];
+        // printf("AAA\n");
+        int tempRow;
+        if (k == 0) {
+            mat1 = b[k];
+            tempRow = dimVect[k];
+            // printf("BBB\n");
+        }
+        else {
+            mat1 = output;
+        }
+        // printf("CCC\n");
+        mat2 = b[k + 1];
 
-		int tempCol = dimVect[k + 2];
-		int commonDim = dimVect[k + 1];
-		output = (int **)malloc(tempRow * sizeof(int *));
-		// printf("DDD\n");
-		for (i = 0; i < dimVect[k]; i++) {
-			output[i] = (int *)malloc(tempCol * sizeof(int));
-		}
-		// printf("EEE\n");
-		for (i = 0; i < tempRow; i++) {
-			for (j = 0; j < tempCol; j++) {
-				int total = 0;
-				// printf("FFF\n");
-				for (int multNum = 0; multNum < commonDim; multNum++) {
-					// printf("GGGG\n");
-					total += (mat1[i][multNum] * mat2[multNum][j]) % MOD;
-				}
-				// printf("HHHH\n");
-				// printf("%d\n", total);
-				// printf("i: %d\n", i);
-				// printf("j: %d\n", j);
-				output[i][j] = total % MOD;
-				// output[i][j] = 0;
-				// printf("IIIII\n");
-			}
-			printf("i: %d\n", i);
-			printf("row: %d\n", tempRow);
-			printf("col: %d\n", tempCol);
-		}
+        int tempCol = dimVect[k + 2];
+        int commonDim = dimVect[k + 1];
+        output = (int **)malloc(tempRow * sizeof(int *));
+        // printf("DDD\n");
+        for (i = 0; i < dimVect[k]; i++) {
+            output[i] = (int *)malloc(tempCol * sizeof(int));
+        }
+        // printf("EEE\n");
+        for (i = 0; i < tempRow; i++) {
+            for (j = 0; j < tempCol; j++) {
+                int total = 0;
+                // printf("FFF\n");
+                for (int multNum = 0; multNum < commonDim; multNum++) {
+                    // printf("GGGG\n");
+                    total += (mat1[i][multNum] * mat2[multNum][j]) % MOD;
+                }
+                // printf("HHHH\n");
+                // printf("%d\n", total);
+                // printf("i: %d\n", i);
+                // printf("j: %d\n", j);
+                output[i][j] = total % MOD;
+                // output[i][j] = 0;
+                // printf("IIIII\n");
+            }
+            printf("i: %d\n", i);
+            printf("row: %d\n", tempRow);
+            printf("col: %d\n", tempCol);
+        }
 
-	}
-	return output;
+    }
+    return output;
 }
 
 int * seqMult2(int * dimVect, int dimNum, int **b) {
-	int i, j, k;
-	int *output = b[0];
-	int *mat1;
-	int *mat2;
-	for (k = 0; k < dimNum - 3; k++) {
-		int tempRow;
-		if (k == 0) {
-			mat1 = b[k];
-			tempRow = dimVect[k];
-		}
-		else {
-			mat1 = output;
-		}
+    int i, j, k;
+    int *output = b[0];
+    int *mat1;
+    int *mat2;
+    for (k = 0; k < dimNum - 3; k++) {
+        int tempRow;
+        if (k == 0) {
+            mat1 = b[k];
+            tempRow = dimVect[k];
+        }
+        else {
+            mat1 = output;
+        }
 
-		mat2 = b[k + 1];
+        mat2 = b[k + 1];
 
-		int tempCol = dimVect[k + 2];
-		int commonDim = dimVect[k + 1];
-		output = (int *)malloc(tempRow * tempCol * sizeof(int));
+        int tempCol = dimVect[k + 2];
+        int commonDim = dimVect[k + 1];
+        output = (int *)malloc(tempRow * tempCol * sizeof(int));
 
-		for (i = 0; i < tempRow * tempCol; i++) {
-			int total = 0;
-			int j = i % tempCol;
+        for (i = 0; i < tempRow * tempCol; i++) {
+            int total = 0;
+            int j = i % tempCol;
 
-			for (int k = 0; k < commonDim; k++) {
-				total += mat1[k + (i / tempCol)*commonDim] * mat2[(k*tempCol + j)];
-			}
+            for (int k = 0; k < commonDim; k++) {
+                total += (mat1[k + (i / tempCol)*commonDim] * mat2[(k*tempCol + j)]) % MOD;
+            }
 
-			output[i] = total;
+            output[i] = total % MOD;
 
-		}
+        }
 
-	}
-	return output;
+    }
+    return output;
 }
 
 int main(int argc, const char * argv[]) {
 
-	// Check to see if there is at least one matrix
-	int i, j, k;
-	int threads = 1024; // Standard number of threads
-	int blocks = 32; // Magic value that results in best running speeds and larger acceptable inputs n
-	printf("YYYYYYYYYYYYY\n");
-	char const* const fileName = argv[1];
-	FILE* file = fopen(fileName, "r");
-	char buffer[2048];
-	int temp_dim;
-	printf("YYYYYYYYYYYYY\n");
-	// replace argc - 1 with dimVectLen
-	int dimVectLen = atoi(fgets(buffer, 2048, file));
-	printf("YYYYYYYYYYYYY\n");
-	// Arary holding all the dimensions
-	int * dimVect = (int *)malloc(dimVectLen * sizeof(int));
-	for (int i = 0; i < dimVectLen; i++) {
-		fscanf(file, "%d", &dimVect[i]);
+    // Check to see if there is at least one matrix
+    int i, j, k;
+    int threads = 1024; // Standard number of threads
+    int blocks = 32; // Magic value that results in best running speeds and larger acceptable inputs n
+    printf("YYYYYYYYYYYYY\n");
+    char const* const fileName = argv[1];
+    FILE* file = fopen(fileName, "r");
+    char buffer[2048];
+    int temp_dim;
+    printf("YYYYYYYYYYYYY\n");
+    // replace argc - 1 with dimVectLen
+    int dimVectLen = atoi(fgets(buffer, 2048, file));
+    printf("YYYYYYYYYYYYY\n");
+    // Arary holding all the dimensions
+    int * dimVect = (int *)malloc(dimVectLen * sizeof(int));
+    for (int i = 0; i < dimVectLen; i++) {
+        fscanf(file, "%d", &dimVect[i]);
+    }
+    printf("YYYYYYYYYYYYY\n");
+    int *a; // Temporary 2D array to hold current matrix
+    int **b = (int **)malloc((dimVectLen) * sizeof(int*)); // Array holding all matrices
+    for (i = 0; i < dimVectLen - 1; i++) {
+        a = createMatrix(file, dimVect[i], dimVect[i + 1]);
+        b[i] = a;
+    }
+
+
+    // for (k = 0; k < dimVectLen - 1; k++) {
+    //         a = b[k];
+    //         int tempRow = dimVect[k];
+    //         int tempCol = dimVect[k + 1];
+    //         for (i = 0; i < tempRow; i++)
+    //             for (j = 0; j < tempCol; j++)
+    //                 printf("%d ", a[i][j]);
+    //         printf("\n");
+    // }
+    printf("YYYYYYYYYYYYY\n");
+    // while(fgets(line, sizeof(line), file)) {
+
+    // }
+
+    // Call Sequential Multiplication
+    int *result = seqMult2(dimVect, dimVectLen, b);
+    printf("ZZZZZZZZZZZZ\n");
+
+
+
+    for (int i = 0; i < dimVect[0] * dimVect[dimVectLen - 1]; i++) {
+        printf("%d ", result[i]);
+    }
+
+    printf("\n");
+    printf("success!!!!\n");
+    fclose(file);
+    // // srand(time(0));
+    // int matrixDims[] = {3,3,3};
+    // int numOfMatrixes = NELE(matrixDims) - 1;
+
+    // double ** matrixArray = (double **)malloc(numOfMatrixes * sizeof(double *));
+    // int max = 3;
+
+    // for (int i = 0; i < numOfMatrixes; i++) {
+    //     matrixArray[i] = createMatrix(matrixDims[i], matrixDims[i + 1]);
+    // }
+
+    // printMatrix(matrixArray[0], 3, 3);
+	int * d_dimVect;
+	cudaMalloc((void **)&d_dimVect, (argc-1) * sizeof(int));
+	cudaMemcpy(d_dimVect, dimVect, (argc - 1) * sizeof(int), cudaMemcpyHostToDevice);
+
+	int ** matList = (int **)malloc((argc-2) * sizeof(int *));
+	for (i = 0; i < (argc-2); i++) {
+		cudaMalloc((void **)&matList[i], dimVect[i] * dimVect[i + 1] * sizeof(int));
+		cudaMemcpy(matList[i], b[i], dimVect[i] * dimVect[i + 1] * sizeof(int), cudaMemcpyHostToDevice);
 	}
-	printf("YYYYYYYYYYYYY\n");
-	int *a; // Temporary 2D array to hold current matrix
-	int **b = (int **)malloc((dimVectLen) * sizeof(int*)); // Array holding all matrices
-	for (i = 0; i < dimVectLen - 1; i++) {
-		a = createMatrix(file, dimVect[i], dimVect[i + 1]);
-		b[i] = a;
+
+	int ** d_matList;
+	cudaMalloc(&d_matList, (argc - 2) * sizeof(int *));
+	cudaMemcpy(d_matList, matList, (argc - 2) * sizeof(int *), cudaMemcpyHostToDevice);
+
+
+	int ** interMat = (int **)malloc(ceil((argc - 2) / 2) + 1 * sizeof(int *));
+	int * currentMat = (int *)calloc(100, sizeof(int));
+	for (i = 0; i < ceil((argc-2)/2)+1; i++) {
+		cudaMalloc((void **)&interMat[i], 100 * sizeof(int));
+		cudaMemcpy(interMat[i], currentMat, 100 * sizeof(int), cudaMemcpyHostToDevice);
 	}
 
+	int ** d_interMat;
+	cudaMalloc(&d_interMat, (argc - 2) * sizeof(int *));
+	cudaMemcpy(d_interMat, interMat, (argc - 2) * sizeof(int *), cudaMemcpyHostToDevice);
 
-	// for (k = 0; k < dimVectLen - 1; k++) {
-	//         a = b[k];
-	//         int tempRow = dimVect[k];
-	//         int tempCol = dimVect[k + 1];
-	//         for (i = 0; i < tempRow; i++)
-	//             for (j = 0; j < tempCol; j++)
-	//                 printf("%d ", a[i][j]);
-	//         printf("\n");
-	// }
-	printf("YYYYYYYYYYYYY\n");
-	// while(fgets(line, sizeof(line), file)) {
+	int levels = log(argc - 2) / log(2);
+	parMat2 << <blocks, threads >> > (d_dimVect, argc, d_matList, d_interMat, levels);
 
-	// }
+	/*
+	int *res = (int *)malloc(100 * 100 * sizeof(int));
+	for (i = 0; i < ceil((argc - 2) / 2) + 1; i++) {
+		printf("1:\n");
+		cudaMemcpy(res, d_interMat[i], 100 * 100 * sizeof(int), cudaMemcpyDeviceToHost);
+		printf("2:\n");
+		b[i] = res;
+	}
+	*/
 
-	// Call Sequential Multiplication
-	int *result = seqMult2(dimVect, dimVectLen, b);
-	printf("ZZZZZZZZZZZZ\n");
+	int res[2][2];
+	for (int i = 0; i<2; i++)
+			cudaMemcpy(&res[i][0], interMat[i], 4*sizeof(int), cudaMemcpyDeviceToHost);
 
+	for (int i = 0; i<2; i++)
+		for (int j = 0; j<4; j++)
+				printf("[%d][%d]=%d\n", i, j, res[i][j]);
 	
-
-	for (int i = 0; i < dimVect[0] * dimVect[dimVectLen - 1]; i++) {
-		printf("%d ", result[i]);
-	}
-
-	printf("\n");
-	printf("success!!!!\n");
-	fclose(file);
-	// // srand(time(0));
-	// int matrixDims[] = {3,3,3};
-	// int numOfMatrixes = NELE(matrixDims) - 1;
-
-	// double ** matrixArray = (double **)malloc(numOfMatrixes * sizeof(double *));
-	// int max = 3;
-
-	// for (int i = 0; i < numOfMatrixes; i++) {
-	//     matrixArray[i] = createMatrix(matrixDims[i], matrixDims[i + 1]);
-	// }
-
-	// printMatrix(matrixArray[0], 3, 3);
 }
